0e94d3ac1ba8af27337ec4b79315d40e1dbfd82b
spi129953-wq <spi129953@gmail.com>
2026-04-13 18:34:59 +0700

    Add handshake sniffer header file with function declarations

--- main/handshake_sniffer.h
+++ main/handshake_sniffer.h
@@ -1,1 +1,13 @@
+#ifndef HANDSHAKE_SNIFFER_H
+#define HANDSHAKE_SNIFFER_H
 
+#include "main.h"
+
+void handshake_sniffer_init(wifi_ap_info_t *target);
+void handshake_sniffer_start(void);
+void handshake_sniffer_stop(void);
+bool handshake_sniffer_is_complete(void);
+void handshake_sniffer_get_status(char *buffer, int buffer_size);
+void handshake_sniffer_get_hex_data(char *buffer, int buffer_size);
+
+#endif
