d3d35d4256fc266b04c08e7192c396c47716a275
spi129953-wq <spi129953@gmail.com>
2026-04-13 18:35:59 +0700

    Add header file for WiFi scanner functionality

--- main/wifi_scanner.h
+++ main/wifi_scanner.h
@@ -1,1 +1,12 @@
+#ifndef WIFI_SCANNER_H
+#define WIFI_SCANNER_H
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "main.h"
+
+void wifi_scanner_init(void);
+int wifi_scanner_scan_all(wifi_ap_info_t *ap_list, int max_ap);
+bool wifi_scanner_select_target_by_index(int index, wifi_ap_info_t *target);
+
+#endif
